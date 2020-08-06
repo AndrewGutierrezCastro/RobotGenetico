3e83e55de75cc572c4fd8fb88964f0000d73e3d4
AndrewGutierrezCastro <andrewgc@estudiantec.cr>
2020-08-05 21:28:26 -0600

    Update Generacion.java

--- src/model/Generacion.java
+++ src/model/Generacion.java
@@ -76,9 +76,9 @@ public class Generacion {
 		}
 		for(int j=0; j<robots.length; j++) {
 			numeroDeEleccion = rand.nextInt(100);
-			for(int k=1; k<robots.length; k++) {
+			for(int k=0; k<probabilidades.length; k++) {
 				if(probabilidades[k] >= numeroDeEleccion && probabilidades[k-1] <= numeroDeEleccion ){
-					robotsElegidos[j] = robots[k].clone();
+					robotsElegidos[j] = robots[j].clone();
 					break;
 				}
 			}
@@ -89,35 +89,40 @@ public class Generacion {
 	public void Cruzar(Robot[] robotsElegidos) {
 		Random rand = new Random();
 		int numOmitido;
+		
 		for(int i = 0; i < robotsElegidos.length; i = i+2) {
 			System.out.println("********************************"+i);
 			System.out.println(robotsElegidos[i]);
 
 			System.out.println(robotsElegidos[i+1]);
-
+			
 			numOmitido = rand.nextInt(3);
 			for(int j=0; j<3; j++) {
 				if(numOmitido != j) {
 					switch(j) {
 						case 0:
 							Hardware bateria = robotsElegidos[i].getCaracteristicas().Bateria;
-							robotsElegidos[i].getCaracteristicas().Bateria = robotsElegidos[i+1].getCaracteristicas().Bateria;
+							robotsElegidos[i].getCaracteristicas().Bateria = 
+									robotsElegidos[i+1].getCaracteristicas().Bateria;
 							robotsElegidos[i+1].getCaracteristicas().Bateria = bateria;
 							break;
 						case 1:
 							Hardware motor = robotsElegidos[i].getCaracteristicas().Motor;
-							robotsElegidos[i].getCaracteristicas().Motor = robotsElegidos[i+1].getCaracteristicas().Motor;
+							robotsElegidos[i].getCaracteristicas().Motor = 
+									robotsElegidos[i+1].getCaracteristicas().Motor;
 							robotsElegidos[i+1].getCaracteristicas().Motor = motor;
 							break;
 						case 2:
 							Hardware camara = robotsElegidos[i].getCaracteristicas().Camara;
-							robotsElegidos[i].getCaracteristicas().Camara = robotsElegidos[i+1].getCaracteristicas().Camara;
+							robotsElegidos[i].getCaracteristicas().Camara = 
+									robotsElegidos[i+1].getCaracteristicas().Camara;
 							robotsElegidos[i+1].getCaracteristicas().Camara = camara;
 							break;
-					}
+					}					
 				}
 			}
 		}
+		
 	}
 
 	public void SetLblTerrenoRobots() {
